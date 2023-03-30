52dc6ba8be420886170a00f3135d552c3779cd53
M Leanca <124058246+mleanca@users.noreply.github.com>
2023-03-30 12:00:28 +0300

    Updated to answer item nr 3

--- Lesson24Ex2And3Of3/MainNumbers.java
+++ Lesson24Ex2And3Of3/MainNumbers.java
@@ -16,5 +16,24 @@ public class MainNumbers {
         //Exception in thread "main" java.lang.IllegalArgumentException: Divisor cannot be equal to 0
         //	at Lesson24Ex2Of3.Numbers.divideBy(Numbers.java:9)
         //	at Lesson24Ex2Of3.MainNumbers.main(MainNumbers.java:14)
+        
+        
+        
+        Numbers count1 = new Numbers();
+        try {
+            System.out.println("Count of numbers divisible by 7 from 1 to 100: " + count1.countDivisibleBy7Numbers(1, 100));
+        } catch (Numbers.InvalidRangeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Numbers count2 = new Numbers();
+        try {
+            System.out.println("Count of numbers divisible by 7 from 100 to 1: " + count2.countDivisibleBy7Numbers(100, 1));
+        } catch (Numbers.InvalidRangeException e) {
+            System.out.println(e.getMessage());
+        }
+        //Count of numbers divisible by 7 from 1 to 100: 14
+        //Invalid Range Exception - inferior limit must be lower than superior limit
+        
     }
 }
